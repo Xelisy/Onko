68605fa68cd54dbeae6fa7f4563c49f6a4a1a643
Xelisy <aag_2003@mail.ru>
2024-12-10 20:52:59 +0300

    новая кнопка

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const buttons = document.querySelectorAll('button');
+// document.addEventListener('DOMContentLoaded', () => {
+//     const buttons = document.querySelectorAll('button');
 
-    function materializeEffect(event) {
-      const circle = document.createElement('div');
-      const rect = event.currentTarget.getBoundingClientRect();
-      const x = event.clientX - rect.left;
-      const y = event.clientY - rect.top;
+//     function materializeEffect(event) {
+//       const circle = document.createElement('div');
+//       const rect = event.currentTarget.getBoundingClientRect();
+//       const x = event.clientX - rect.left;
+//       const y = event.clientY - rect.top;
 
-      circle.classList.add('circle');
-      circle.style.left = `${x}px`;
-      circle.style.top = `${y}px`;
-      event.currentTarget.appendChild(circle);
+//       circle.classList.add('circle');
+//       circle.style.left = `${x}px`;
+//       circle.style.top = `${y}px`;
+//       event.currentTarget.appendChild(circle);
 
-      setTimeout(() => circle.remove(), 500);
+//       setTimeout(() => circle.remove(), 500);
     
-    setTimeout(() => {
-        window.location.href = 'login.html'
-        }, 300)
-    }
+//     setTimeout(() => {
+//         window.location.href = 'login.html'
+//         }, 300)
+//     }
 
-    buttons.forEach(button => {
-        button.addEventListener('click', materializeEffect); 
-    });
-});
+//     buttons.forEach(button => {
+//         button.addEventListener('click', materializeEffect); 
+//     });
+// });

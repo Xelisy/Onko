e20c17664198d3623c2a159037fd23fdbcffe428
Xelisy <aag_2003@mail.ru>
2024-12-19 19:43:21 +0300

    смерть

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const buttons = document.querySelectorAll('button');
-
-//     function materializeEffect(event) {
-//       const circle = document.createElement('div');
-//       const rect = event.currentTarget.getBoundingClientRect();
-//       const x = event.clientX - rect.left;
-//       const y = event.clientY - rect.top;
-
-//       circle.classList.add('circle');
-//       circle.style.left = `${x}px`;
-//       circle.style.top = `${y}px`;
-//       event.currentTarget.appendChild(circle);
-
-//       setTimeout(() => circle.remove(), 500);
+let inputJs = document.querySelectorAll('.inputCont')
+inputJs.forEach(item => {
+    item.addEventListener('input', () => {
+        if (item.children[0].value) {
+            item.children[1].className = 'active'
+        }
+    })
+    if (item.children[2]) {
+        item.children[2].addEventListener('click', () => {
+            console.log('ihui beka ishak')
+            if(item.children[0].getAttribute('type') == 'password') {
+                item.children[0].setAttribute('type', '')
+            }
+            else {
+                item.children[0].setAttribute('type', 'password')
+            }
+            item.children[2].children[0].classList.toggle('offImg')
+            item.children[2].children[1].classList.toggle('offImg')
+        })
+    }
     
-//     setTimeout(() => {
-//         window.location.href = 'login.html'
-//         }, 300)
-//     }
-
-//     buttons.forEach(button => {
-//         button.addEventListener('click', materializeEffect); 
-//     });
-// });
+})
